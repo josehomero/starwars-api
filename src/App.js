6d9f2b2c4ec530c057cf4cb809adacee82acd9eb
josehomero <jose.homero.rios@outlook.com>
2020-08-27 22:03:07 -0700

    learning about requests

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Button from 'react-bootstrap/Button';
+import axios from 'axios'
 
 function App() {
+
+  componentDidMount() {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Click me</Button>
-      </header>
-x
+      <h1>App</h1>
     </div>
   );
 }
