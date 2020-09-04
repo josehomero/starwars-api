e42d22812df1e9ab9a2f5a94a5c400b994eff048
josehomero <jose.homero.rios@outlook.com>
2020-09-03 21:51:02 -0700

    making progress, first character is in the table

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ import Table from './components/Table'
 import axios from 'axios'
 
 class App extends React.Component {
-
   constructor() {
-    super();
+    super()
     this.state = {
+      character:{}
     }
   }
 
-
+componentDidMount(){
+  fetch("https://swapi.dev/api/people/1/")
+  .then(response => response.json())
+  .then(data => {
+    this.setState({
+      character: data
+    })
+  })
+}
 
   render() {
     return(
       <div>
         <Header />
-        <Table />
+        <Table characters={this.state.character} />
       </div>
     )
   }
--- src/components/Table.js
+++ src/components/Table.js
@@ -4,7 +4,28 @@ class Table extends React.Component {
     render(){
         return(
             <div>
-                <h2>hello </h2>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Birth Date</th>
+                            <th>Height</th>
+                            <th>Mass</th>
+                            <th>Home World</th>
+                            <th>Species</th>
+                        </tr>
+                    </thead>    
+                    <tbody>
+                        <tr>
+                            <td>{this.props.characters.name}</td>
+                            <td>{this.props.characters.birth_year}</td>
+                            <td>{this.props.characters.height}</td>
+                            <td>{this.props.characters.mass}</td>
+                            <td>{this.props.characters.homeworld}</td>
+                            <td>{this.props.characters.species}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
